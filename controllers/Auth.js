8c49b7d8a70abb50fc22ae6375aee1f885341c4b
Medina88 <Msydyka@iu.edu>
2021-09-10 22:51:12 -0400

    Add: Bcrypt support for passwords

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,6 +2,7 @@ let express = require('express')
 let router = express.Router()
 const { User } = require('../models')
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 router.post("/register/", async (req, res) => {
     let message 
@@ -9,7 +10,7 @@ router.post("/register/", async (req, res) => {
     try {
         const user = await User.create({
             username: req.body.user.username,
-            password: req.body.user.password,
+            password: bcrypt.hashSync(req.body.user.password, 13),
             firstname: req.body.user.firstname,
             lastname: req.body.user.lastname,
             email: req.body.user.email,
@@ -45,16 +46,25 @@ router.post("/login/", async (req, res) => {
 
         if (loginUser){
 
-            let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, {expiresIn: 6*60*24});
+            let passwordComparison = await bcrypt.compare(password, loginUser.password);
+
+            if(passwordComparison){
+                let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, {expiresIn: 6*60*24});
 
             res.status(200).json({
                 user: loginUser,
                 message: "User successfully logged in!",
                 sessionToken: token
             });
+            } else{
+                res.status(401).json({
+                    message: "Incorrect email or password"
+                });
+            }
+            
         } else{
             res.status(401).json({
-                message: "Login Failed!"
+                message: "Incorrect email or password"
             });
         }
     } catch (err){
