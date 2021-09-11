01232e8b9ee761f871be43aad77216173de866e2
Medina88 <Msydyka@iu.edu>
2021-09-10 21:48:21 -0400

    Add: JWT Support

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let router = express.Router()
 const { User } = require('../models')
-
+const jwt = require("jsonwebtoken")
 
 router.post("/register/", async (req, res) => {
     let message 
@@ -15,9 +15,13 @@ router.post("/register/", async (req, res) => {
             email: req.body.user.email,
             role: "general"
         })
+
+        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 6*60*24});
+
         message = {
             msg:'User Registered', 
-            user
+            user: user,
+            sessionToken: token
         }
     } catch (err){
         //console.log(err)
@@ -40,9 +44,13 @@ router.post("/login/", async (req, res) => {
         });
 
         if (loginUser){
+
+            let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, {expiresIn: 6*60*24});
+
             res.status(200).json({
                 user: loginUser,
-                message: "User successfully logged in!"
+                message: "User successfully logged in!",
+                sessionToken: token
             });
         } else{
             res.status(401).json({
